e0b5c036d8cad9e3dddb9d0834c91352c2a501d5
Martin Rafael Gonzalez <tin@devtin.io>
2020-01-13 14:12:35 -0500

    add Array type tests

--- test/types/array.test.js
+++ test/types/array.test.js
@@ -1,7 +1,38 @@
 import test from 'ava'
 import { Schema } from '../../'
 
-test(`Arrays can describe the type of items they store, initializing them.`, t => {
+test(`Parses arrays`, t => {
+  const ProductType = new Schema({
+    name: String,
+    category: {
+      type: Array,
+      required: true
+    }
+  })
+
+  const product = ProductType.parse({
+    name: 'Kombucha',
+    category: ['Beverages', 'Tea', 'Health']
+  })
+
+  t.true(Array.isArray(product.category))
+  t.is(product.category.length, 3)
+  t.is(product.category[1], 'Tea')
+
+  try {
+    ProductType.parse({
+      name: 'Kombucha',
+      category: 'none'
+    })
+    t.fail(`Invalid array was resolved!`)
+  } catch (err) {
+    t.is(err.message, `Data is not valid`) // => Data is not valid
+    t.is(err.errors[0].message, 'Invalid array') // => Invalid date
+    t.is(err.errors[0].field.fullPath, 'category')
+  }
+})
+
+test(`Optionally describes the type of items to store, initializing them.`, t => {
   const Log = new Schema({
     user: String,
     lastAccess: {
@@ -18,10 +49,10 @@ test(`Arrays can describe the type of items they store, initializing them.`, t =
     lastAccess: ['6/11/2019', 'Sat Jan 11 2020 17:06:31 GMT-0500 (Eastern Standard Time)']
   })
 
-  t.true(Array.isArray(tinLog.lastAccess)) // => true
-  t.is(tinLog.lastAccess.length, 2) // => 2
-  t.true(tinLog.lastAccess[0] instanceof Date) // => true
-  t.true(tinLog.lastAccess[1] instanceof Date) // => true
+  t.true(Array.isArray(tinLog.lastAccess))
+  t.is(tinLog.lastAccess.length, 2)
+  t.true(tinLog.lastAccess[0] instanceof Date)
+  t.true(tinLog.lastAccess[1] instanceof Date)
 
   try {
     Log.parse({
@@ -30,8 +61,8 @@ test(`Arrays can describe the type of items they store, initializing them.`, t =
     })
     t.fail(`Invalid date was resolved!`)
   } catch (err) {
-    t.is(err.message, `Data is not valid`) // => Data is not valid
-    t.is(err.errors[0].message, 'Invalid date') // => Invalid date
-    t.is(err.errors[0].field.path) // => lastAccess.1
+    t.is(err.message, `Data is not valid`)
+    t.is(err.errors[0].message, 'Invalid date')
+    t.is(err.errors[0].field.fullPath, 'lastAccess.1')
   }
 })
