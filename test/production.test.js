2620cdaab8e32e5b29cb02ab16f066b39eb0f424
Martin Rafael Gonzalez <tin@devtin.io>
2020-01-28 08:09:07 -0500

    test: remove no longer needed test files

--- test/production.test.js
+++
@@ -1,9 +0,0 @@
-import test from 'ava'
-import fs from 'fs'
-import path from 'path'
-
-const { checkMdJsCode } = require('./utils/test-readme.js')
-
-test.skip(`Why runs good`, async t => {
-  t.true(await checkMdJsCode(fs.readFileSync(path.join(__dirname, '../WHY.md')).toString()))
-})
