f4291f2c44f26d80d5a0207ae6012b4747f13ad8
Martin Rafael Gonzalez <tin@devtin.io>
2020-04-01 09:42:04 -0400

    test: add propertiesRestricted unit test

---
+++ src/utils/properties-restricted.unit.js
@@ -0,0 +1,19 @@
+import { propertiesRestricted } from './properties-restricted.js'
+import test from 'ava'
+
+test(`Properties restricted`, t => {
+
+  const user = {
+    name: 'Martin Rafael',
+    email: 'tin@devtin.io',
+    address: {
+      city: 'Miami, Fl',
+      zip: 33129,
+      line1: 'Brickell Ave'
+    }
+  }
+  t.false(propertiesRestricted(user, ['name'])) // => false
+  t.true(propertiesRestricted(user, ['name', 'email', 'address'])) // => true
+  t.true(propertiesRestricted(user, ['name', 'email', 'address.city', 'address.zip', 'address.line1', 'address.line2'])) // => true
+  t.false(propertiesRestricted(user, ['name', 'email', 'address.city', 'address.zip', 'address.line1', 'address.line2'], { strict: true })) // => false
+})
