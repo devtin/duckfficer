8ee4b67222ad1d6a29584901ef69b5e1b80d3113
Martin Rafael Gonzalez <tin@devtin.io>
2020-08-24 10:22:42 -0400

    fix(Object.mapSchema): do not mutate object

--- src/lib/transformers.js
+++ src/lib/transformers.js
@@ -288,6 +288,7 @@ export const Transformers = {
     },
     parse (value) {
       if (this.settings.mapSchema !== undefined) {
+        const newVal = {}
         Object.keys(value).forEach(name => {
           const obj = value[name]
           const schema = this.constructor.castSchema(this.settings.mapSchema)
@@ -303,8 +304,9 @@ export const Transformers = {
               parent: this
             }))
 
-          value[name] = parser.parse(obj)
+          newVal[name] = parser.parse(obj)
         })
+        return newVal
       }
 
       return value
