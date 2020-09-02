4af66def4917f54181f174295c28919644a614d6
Martin Rafael Gonzalez <tin@devtin.io>
2020-09-02 16:27:25 -0400

    feat(utils): export PromiseEach

--- src/utils/promise-each.js
+++ src/utils/promise-each.js
@@ -1,3 +1,17 @@
-export const PromiseEach = async function (arr, fn) {
-  for (const item of arr) await fn(item)
+/**
+ * Loops through given `arr` of functions, awaiting for each result, alternatively breaking the loop when `breakOnFalse`
+ * equals `true` and one of the functions returns `false` explicitly.
+ *
+ * @param {Function[]} arr
+ * @param {Function} fn - callback function to pass iterated items
+ * @param {Boolean} [breakOnFalse=false] - whether to stop the loop when false (explicitly) returned
+ * @return {Promise<void>}
+ */
+
+export const PromiseEach = async function (arr, fn, breakOnFalse = false) {
+  for (const item of arr) {
+    if (await fn(item) === false && breakOnFalse) {
+      break
+    }
+  }
 }
