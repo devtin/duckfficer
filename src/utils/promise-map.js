b1d9606050994955c3e5d9a2e6c7cca1968bdd57
Martin Rafael Gonzalez <tin@devtin.io>
2020-09-02 16:27:08 -0400

    docs(utils): add missing descriptions

--- src/utils/promise-map.js
+++ src/utils/promise-map.js
@@ -1,10 +1,19 @@
 import { PromiseEach } from './promise-each'
 
-export const PromiseMap = async function (arr, fn) {
+/**
+ * Loops through given `arr` of functions, awaiting for each result.
+ *
+ * @param {Function[]} arr
+ * @param {Function} fn - callback function to pass iterated items
+ * @param {Boolean} [breakOnFalse=false] - whether to stop the loop when false (explicitly) returned
+ * @return {Promise<Array>} - array of results
+ */
+
+export const PromiseMap = async function (arr, fn, breakOnFalse) {
   const newArr = []
   let index = 0
   await PromiseEach(arr, async (item) => {
     newArr.push(await fn(item, index++))
-  })
+  }, breakOnFalse)
   return newArr
 }
